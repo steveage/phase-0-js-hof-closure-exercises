31f2e2b14fd9c42d9373fe5f574e317326fcab74
Mariusz <mariuszbia@yahoo.com>
2022-04-05 22:49:57 -0500

    Completed assignment.

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-function forEach() {
+const _ = require("underscore")
 
+function forEach(myArray, callBack) {
+  myArray.forEach(callBack);
 }
 
-function map() {
-
+function map(myArray, callback) {
+  return myArray.map(callback);
 }
 
-function filter() {
-
+function filter(myArray, callBack) {
+  return myArray.filter(callBack);
 }
 
-function every() {
-
+function every(myArray, callBack) {
+  return myArray.every(callBack);
 }
 
-function some() {
-
+function some(myArray, callBack) {
+  return myArray.some(callBack);
 }
 
-function majority() {
-
+function majority(myArray, callBack) {
+  const newArray = myArray.map(callBack);
+  const trueElements = newArray.filter(x=>x===true);
+  const falseElements = newArray.filter(x=>x===false);
+  return trueElements.length>falseElements.length;
 }
 
-function once() {
-
+function once(callBack) {
+  let executeCallBack = _.once = function(callBack) {
+    let functionHasAlreadyRan, result;
+    return function() {
+    if (functionHasAlreadyRan) return result;
+    functionHasAlreadyRan = true;
+    result = callBack();
+    return result;
+    }
+  }
+return executeCallBack(callBack);
 }
 
-function groupBy() {
-
+function groupBy(array, iteratee) {
+  return _.groupBy(array, iteratee);
 }
 
-function arrayToObject() {
-
+function arrayToObject(array, callBack) {
+  let result = {};
+  for(let i=0; i<array.length; ++i){
+    result[array[i]] = callBack(array[i]);
+  }
+  return result;
 }
 
 module.exports = { 
